e1ba02cf6f0970c7da4f5da2326e4139cedab1b8
tonderaimuchada <tonderaimuchada@gmail.com>
2019-07-24 23:55:09 +0200

    Debugging login button click event

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,19 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  loginClicked = function(){
-    console.log("app.component.html")
-    location.href = "app.component.html"
+  loginClicked(){
+    console.log("app.component.html");
+    location.href = "app.component.html";
   }
 }
